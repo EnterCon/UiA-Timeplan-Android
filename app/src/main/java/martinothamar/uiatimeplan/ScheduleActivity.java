8364637e2954f02e0da1e59cf9a220806d162423
Martin Othamar <martin@othamar.net>
2015-09-03 09:01:00 +0200

    Implemented UI for schedule, but the event text is buggy

--- app/src/main/java/martinothamar/uiatimeplan/ScheduleActivity.java
+++ app/src/main/java/martinothamar/uiatimeplan/ScheduleActivity.java
@@ -2,23 +2,40 @@ package martinothamar.uiatimeplan;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.alamkanak.weekview.DateTimeInterpreter;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 import martinothamar.uiatimeplan.Models.Day;
 import martinothamar.uiatimeplan.Models.Programme;
 import martinothamar.uiatimeplan.Models.Week;
 
-public class ScheduleActivity extends Activity {
+public class ScheduleActivity extends Activity implements WeekView.MonthChangeListener,
+        WeekView.EventClickListener, WeekView.EventLongPressListener  {
     public Programme activeProgramme;
+    private static final int TYPE_DAY_VIEW = 1;
+    private static final int TYPE_THREE_DAY_VIEW = 2;
+    private static final int TYPE_WEEK_VIEW = 3;
+    private int mWeekViewType = TYPE_THREE_DAY_VIEW;
+    private WeekView mWeekView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,30 +43,92 @@ public class ScheduleActivity extends Activity {
         setContentView(R.layout.activity_schedule);
         Intent intent = getIntent();
         this.activeProgramme = (Programme)intent.getSerializableExtra("activeProgramme");
-        TableLayout table = (TableLayout)findViewById(R.id.table);
+        // Get a reference for the week view in the layout.
+        mWeekView = (WeekView) findViewById(R.id.weekView);
 
+        // Show a toast message about the touched event.
+        mWeekView.setOnEventClickListener(this);
+
+        // The week view has infinite scrolling horizontally. We have to provide the events of a
+        // month every time the month changes on the week view.
+        mWeekView.setMonthChangeListener(this);
+
+        // Set long press listener for events.
+        mWeekView.setEventLongPressListener(this);
+
+
+        // Set up a date time interpreter to interpret how the date and time will be formatted in
+        // the week view. This is optional.
+        setupDateTimeInterpreter(false);
+
+
+    }
+
+
+    @Override
+    public List<WeekViewEvent> onMonthChange(int i, int i1) {
+        List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
         for (Week week : activeProgramme.getSchedule()){
-            TableRow weekRow = new TableRow(this);
-            TextView textText = new TextView(this);
-            textText.setText("Ukenummer " + week.weekNumber);
-            weekRow.addView(textText);
-            table.addView(weekRow);
             for(Day day : week.days) {
                 for(martinothamar.uiatimeplan.Models.Activity activity : day.activities) {
-                    DateFormat dateFormat = new SimpleDateFormat("HH:mm");
-
-                    TableRow activityRow = new TableRow(this);
-                    TextView time = new TextView(this); time.setText(dateFormat.format(activity.getStart().getTime()) + "-" +
-                            dateFormat.format(activity.getEnd().getTime())); activityRow.addView(time);
-                    TextView course = new TextView(this); course.setText(activity.getCourses().get(0)); activityRow.addView(course);
-                    TextView lecturer = new TextView(this); lecturer.setText(activity.getLecturer()); activityRow.addView(lecturer);
-                    TextView room = new TextView(this); time.setText(activity.getRooms().get(0)); activityRow.addView(room);
-
-                    table.addView(activityRow);
+                    WeekViewEvent event = new WeekViewEvent(1, getEventTitle(activity), activity.getStart(), activity.getEnd());
+                    event.setColor(getResources().getColor(R.color.primary_light, this.getTheme()));
+                    events.add(event);
                 }
             }
         }
+        return events;
     }
+
+
+    private String getEventTitle(martinothamar.uiatimeplan.Models.Activity activity) {
+        return String.format(activity.getCourses().get(0) + " %02d:%02d til %02d:%02d",
+                activity.start.get(Calendar.HOUR_OF_DAY), activity.end.get(Calendar.MINUTE),
+                activity.end.get(Calendar.HOUR_OF_DAY), activity.end.get(Calendar.MINUTE));
+    }
+
+
+    @Override
+    public void onEventClick(WeekViewEvent weekViewEvent, RectF rectF) {
+
+    }
+
+    @Override
+    public void onEventLongPress(WeekViewEvent weekViewEvent, RectF rectF) {
+
+    }
+
+
+
+    /**
+     * Set up a date time interpreter which will show short date values when in week view and long
+     * date values otherwise.
+     * @param shortDate True if the date values should be short.
+     */
+    private void setupDateTimeInterpreter(final boolean shortDate) {
+        mWeekView.setDateTimeInterpreter(new DateTimeInterpreter() {
+            @Override
+            public String interpretDate(Calendar date) {
+                SimpleDateFormat weekdayNameFormat = new SimpleDateFormat("EEE", Locale.getDefault());
+                String weekday = weekdayNameFormat.format(date.getTime());
+                SimpleDateFormat format = new SimpleDateFormat(" dd.MM", Locale.getDefault());
+
+                // All android api level do not have a standard way of getting the first letter of
+                // the week day name. Hence we get the first char programmatically.
+                // Details: http://stackoverflow.com/questions/16959502/get-one-letter-abbreviation-of-week-day-of-a-date-in-java#answer-16959657
+                if (shortDate)
+                    weekday = String.valueOf(weekday.charAt(0));
+                return weekday.toUpperCase() + format.format(date.getTime());
+            }
+
+            @Override
+            public String interpretTime(int hour) {
+                return Integer.toString(hour);
+            }
+        });
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,16 +139,52 @@ public class ScheduleActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        setupDateTimeInterpreter(id == R.id.action_week_view);
+        switch (id){
+            case R.id.action_today:
+                mWeekView.goToToday();
+                return true;
+            case R.id.action_day_view:
+                if (mWeekViewType != TYPE_DAY_VIEW) {
+                    item.setChecked(!item.isChecked());
+                    mWeekViewType = TYPE_DAY_VIEW;
+                    mWeekView.setNumberOfVisibleDays(1);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                    // Lets change some dimensions to best fit the view.
+                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics()));
+                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
+                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
+                }
+                return true;
+            case R.id.action_three_day_view:
+                if (mWeekViewType != TYPE_THREE_DAY_VIEW) {
+                    item.setChecked(!item.isChecked());
+                    mWeekViewType = TYPE_THREE_DAY_VIEW;
+                    mWeekView.setNumberOfVisibleDays(3);
+
+                    // Lets change some dimensions to best fit the view.
+                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics()));
+                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
+                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, getResources().getDisplayMetrics()));
+                }
+                return true;
+            case R.id.action_week_view:
+                if (mWeekViewType != TYPE_WEEK_VIEW) {
+                    item.setChecked(!item.isChecked());
+                    mWeekViewType = TYPE_WEEK_VIEW;
+                    mWeekView.setNumberOfVisibleDays(7);
+
+                    // Lets change some dimensions to best fit the view.
+                    mWeekView.setColumnGap((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics()));
+                    mWeekView.setTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 10, getResources().getDisplayMetrics()));
+                    mWeekView.setEventTextSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 10, getResources().getDisplayMetrics()));
+                }
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
